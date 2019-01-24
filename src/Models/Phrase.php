93e86c54bdc98e498afd2cc2aac9b34afcec241f
Martin Blagoev <mb.reppair@gmail.com>
2019-01-24 19:43:53 +0200

    fix bug when passing specific locale to __('db.something', [], 'locale')

--- src/Models/Phrase.php
+++ src/Models/Phrase.php
@@ -2,6 +2,7 @@
 
 namespace Oxygencms\Phrases\Models;
 
+use function foo\func;
 use Oxygencms\Core\Models\Model;
 use Spatie\Translatable\HasTranslations;
 
@@ -27,11 +28,16 @@ class Phrase extends Model
      * @param string $locale
      * @return array
      */
-    public static function getGroup(string $group, string $locale): array
+    public static function getGroup(string $group, string $locale = null)
     {
-        return static::query()
-                     ->where('group', $group)
-                     ->pluck('text', 'key')
-                     ->all();
+        $query = static::query()->where('group', $group);
+
+        if (is_null($locale)) {
+            return $query->pluck('text', 'key')->all();
+        }
+
+        return $query->get()->flatMap(function ($phrase) use ($locale) {
+            return [$phrase->key => $phrase->getTranslation('text', $locale)];
+        })->all();
     }
 }
